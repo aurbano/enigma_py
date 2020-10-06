d57b9428c5a517ee2d658727d1b20e3189386b84
Alejandro U. Alvarez <aluralma@gmail.com>
2020-10-06 20:12:30 +0100

    add enigma files

---
+++ enigma.py
@@ -0,0 +1,22 @@
+
+class PlugLead:
+    def __init__(self, mapping):
+        # Your code here
+        raise NotImplementedError()
+
+    def encode(self, character):
+        # Your code here
+        raise NotImplementedError()
+
+
+class Plugboard:
+    # Your code here
+    raise NotImplementedError()
+
+
+# You will need to write more classes, which can be done here or in separate files, you choose.
+
+
+if __name__ == "__main__":
+    # You can use this section to write tests and demonstrations of your enigma code.
+    pass
