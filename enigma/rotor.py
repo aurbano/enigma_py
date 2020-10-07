cd973617c42340424cda5cdcb3e90855394aa059
Alejandro Alvarez <alejandro.alvarez@transferwise.com>
2020-10-07 15:06:25 +0100

    cleanup rotor

--- enigma/rotor.py
+++ enigma/rotor.py
@@ -19,26 +19,15 @@ class Rotor:
         self.setting = max(0, min(self.char_range, setting - 1))
 
     def encode_right_to_left(self, char: str):
-        current_char = ord(char) - self.initial_position
-
-        if current_char < 0 or current_char > self.char_range:
-            return char
-
-        # get index of char in rotor accounting for rotation
-        entry_index = self._offset_in(current_char)
-
-        # return encoded char
-        output_index = ord(self.config[entry_index]) - self.initial_position
-
-        # return character at label accounting for rotation
-        return chr(self._offset_out(output_index) + self.initial_position)
+        in_contact_index = self._get_contact_in(char)
+        pin_index = ord(self.config[in_contact_index]) - self.initial_position
+        return self._get_pin_out(pin_index)
 
     def encode_left_to_right(self, char: str):
-        contact_index = self._offset_in(ord(char) - self.initial_position)
-        contact_char = chr(contact_index + self.initial_position)
-        entry_index = self.config.index(contact_char)
-
-        return chr(self._offset_out(entry_index) + self.initial_position)
+        in_contact_index = self._get_contact_in(char)
+        contact_char = chr(in_contact_index + self.initial_position)
+        pin_index = self.config.index(contact_char)
+        return self._get_pin_out(pin_index)
 
     def rotate(self):
         self.position = (self.position + 1) % (self.char_range + 1)
@@ -51,6 +40,13 @@ class Rotor:
 
     def current_position(self):
         return chr(self.position + self.initial_position)
+
+    def _get_contact_in(self, char: str):
+        in_contact_index = ord(char) - self.initial_position
+        return self._offset_in(in_contact_index)
+
+    def _get_pin_out(self, index: int):
+        return chr(self._offset_out(index) + self.initial_position)
 
     def _offset_in(self, char: int):
         return (char + self.position - self.setting) % (self.char_range + 1)
